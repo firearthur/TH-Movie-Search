1bf57931961b80cdb52036f2008681a8e9c7a5c5
fireArthur <thefirearthur@gmail.com>
2018-06-19 21:07:05 -0700

    [style] styles the navbar

--- client/src/app/navbar/navbar.component.ts
+++ client/src/app/navbar/navbar.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
-
-  constructor() { }
+  searchSelected: boolean;
+  historySelected: boolean;
+  constructor(private router: Router) { }
 
   ngOnInit() {
+  }
+  ngAfterContentChecked() {
+    if (this.router.url === '/search') {
+      this.searchSelected = true;
+      this.historySelected = false;
+    } else if (this.router.url === '/history') {
+      this.searchSelected = false;
+      this.historySelected = true;
+    }
   }
 
 }
