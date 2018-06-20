506ff4aa0bffb3c8510886bcb200f919d7ce6aa7
fireArthur <thefirearthur@gmail.com>
2018-06-20 12:37:20 -0700

    [Deployment] fixes mongodb uri

--- db/index.js
+++ db/index.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 
-mongoose.connect(process.env.DB_LINK);
+mongoose.connect(process.env.MONGODB_URI || process.env.DB_LINK);
 
 const db = mongoose.connection;
 
